468913a33d2fa24b4d444c84d3de2cb4a7a609d0
Kerry Smith <kerrysfs@gmail.com>
2020-09-19 15:09:37 -0700

    added srcDoc made function to setimeout delay for render of scripts, us

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,62 @@
-import React, { useState } from 'react';
-import Editor from './Editor'
+import React, { useState, useEffect } from "react";
+import Editor from "./Editor";
 
 function App() {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
 
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+        </html>
+        `);
+    }, 250);
+  }, [html, css, js]);
 
+  // const srcDoc =
 
- return (
-  <>
-  <div className="pane top-pane">
-    <Editor
-     language="xml" 
-    displayName="HTML" 
-    value={html} 
-    onChange={setHtml}
-    
-    />
-   
-    <Editor
-     language="css"
-      displayName="CSS"
-       value={css}
-        onChange={setCss}
-        
-        />
-   
-    <Editor
-     language="javascript"
-      displayName="Javascript"
-       value={js}
-        onChange={setJs}
-        
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="- HTML -"
+          value={html}
+          onChange={setHtml}
         />
 
-  </div>
-  <div className="pane">
-    <iframe
-      title="output"
-      sandbox="allow-scripts"
-      frameBorder="0"
-      width="100%"
-      height="100%"
+        <Editor
+          language="css"
+          displayName="- CSS -"
+          value={css}
+          onChange={setCss}
+        />
 
-
-    />
-
-   
-  </div>
-  
-  
-  </>
- )
+        <Editor
+          language="javascript"
+          displayName="- JavaScript -"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
